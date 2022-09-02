afb79102018a4cea3d74fe8df5d42d59f69713c6
Gregor Schaefer <gregor.schaefer@de.bosch.com>
2022-09-02 09:43:26 +0200

    Add schema validation

--- parse_json.py
+++ parse_json.py
@@ -1,5 +1,17 @@
 import json
 from pathlib import Path
+import jsonschema
+from jsonschema import validate
+
+configSchema = {
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "version": {"type": "number"},
+        "path": {"type": "string"},
+    },
+    "required": ["name", "version"]
+}
 
 def parse_json(filepath):
   if not Path(filepath).is_file():
@@ -21,6 +33,13 @@ def is_json(file):
     return False
   return True
 
+def validateSchema(jsonData):
+    try:
+        validate(instance=jsonData, schema=configSchema)
+    except jsonschema.exceptions.ValidationError as err:
+        print(err)
+        return False
+    return True
 
 (success, value) = parse_json("assets/not_existing.json")
 print(success)
@@ -37,3 +56,21 @@ print(value)
 (success, value) = parse_json("assets/valid.json")
 print(success)
 print(value)
+
+print("##### VALIDATE SCHEMA #####")
+
+jsonData = json.loads('{"name": "config1", "number": 1, "path": "72"}')
+isValid = validateSchema(jsonData)
+print(isValid)
+
+jsonData = json.loads('{"name": "config2", "version": "2", "path": 72}')
+isValid = validateSchema(jsonData)
+print(isValid)
+
+jsonData = json.loads('{"name": "config2", "version": 3, "path": "pathToFile"}')
+isValid = validateSchema(jsonData)
+print(isValid)
+
+jsonData = json.loads('{"name": "config2", "version": 3}')
+isValid = validateSchema(jsonData)
+print(isValid)
